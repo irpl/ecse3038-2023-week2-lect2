9c6cbd049b4e3361f9291b0f57ebf71a46d4f141
Phillip Logan <philo3194@gmail.com>
2023-01-24 09:35:08 -0500

    added multiplication on line 8 and added print statement to end of file

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ def area_of_circle(r):
   return a
 
 def volume_of_sphere(r):
-  v = (4(math.pi * r**3))/3
+  v = (4*(math.pi * r**3))/3
   return v
 
 print(area_of_circle(5))
+print(volume_of_sphere(56))
