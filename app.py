a61ffaf7f520962d008e80bc8d683af8ae7a0e35
Phillip Logan <philo3194@gmail.com>
2023-01-24 09:40:56 -0500

    added pythag function and added print statement

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def volume_of_sphere(r):
   v = (4*(math.pi * r**3))/3
   return v
 
+def pythag(a, b):
+  c = math.sqrt(a**2 + b**2)
+  return c
+
+
 print(area_of_circle(5))
 print(volume_of_sphere(56))
+print(pythag(2,3))
