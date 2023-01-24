d87d888b2837225aa234e3ac268d89203ca673f7
Phillip Logan <philo3194@gmail.com>
2023-01-24 09:53:04 -0500

    added summation function and last print statement

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ def pythag(a, b):
   c = math.sqrt(a**2 + b**2)
   return c
 
+def summation():
+  result = 0
+
+  for n in range(1, 21):
+    result += math.sqrt(((n + 5)**5)/2)
+  
+  return result
+
 
 print(area_of_circle(5))
 print(volume_of_sphere(56))
 print(pythag(2,3))
+print(summation())
